5f2c004e1e073977239baa04860f678e3e542109
Polina <91316935+Polinalavender@users.noreply.github.com>
2025-02-19 16:49:24 +0100

    Add changeState method to SmartCamera

--- src/src/SmartCamera.java
+++ src/src/SmartCamera.java
@@ -1,2 +1,11 @@
-public class SmartCamera {
+public class SmartCamera extends SmartDevice {
+    public SmartCamera(String deviceName) {
+        super(deviceName);
+    }
+
+    @Override
+    public void changeState(String newState) {
+        this.status = newState.equalsIgnoreCase("RECORDING") ? "RECORDING" : "IDLE";
+        notifyObservers();
+    }
 }
