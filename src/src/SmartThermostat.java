5594265bf70f71b9cc246b18ddc3366e520a737a
Polina <91316935+Polinalavender@users.noreply.github.com>
2025-02-19 16:48:07 +0100

    Add change state to SmartThermostat

--- src/src/SmartThermostat.java
+++ src/src/SmartThermostat.java
@@ -1,2 +1,11 @@
-public class SmartThermostat {
+public class SmartThermostat extends SmartDevice {
+    public SmartThermostat(String deviceName) {
+        super(deviceName);
+    }
+
+    @Override
+    public void changeState(String newState) {
+        this.status = newState + "°C";
+        notifyObservers();
+    }
 }
