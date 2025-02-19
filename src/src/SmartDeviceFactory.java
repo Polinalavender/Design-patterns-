c0b25eda20545e651ce1303a92e02ef67f5ac4a6
Polina <91316935+Polinalavender@users.noreply.github.com>
2025-02-19 16:50:59 +0100

    SmartDeviceFactory method

--- src/src/SmartDeviceFactory.java
+++ src/src/SmartDeviceFactory.java
@@ -1,2 +1,10 @@
 public class SmartDeviceFactory {
+    public static SmartDevice createDevice(String type, String name) {
+        return switch (type.toLowerCase()) {
+            case "light" -> new SmartLight(name);
+            case "thermostat" -> new SmartThermostat(name);
+            case "camera" -> new SmartCamera(name);
+            default -> throw new IllegalArgumentException("Unknown device type: " + type);
+        };
+    }
 }
