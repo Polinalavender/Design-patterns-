f78bed0f4aebafaf7b09c04474459782c71cfcc4
liafernando <91317153+liafernando@users.noreply.github.com>
2025-03-18 09:51:41 +0100

    Updated the errors with package for smart light

--- Smart-Home-App/src/main/java/device/SmartLight.java
+++ Smart-Home-App/src/main/java/device/SmartLight.java
@@ -1,4 +1,20 @@
-package device;
+package com.smarthome.model.device;
 
-public class SmartLight {
+public class SmartLight extends SmartDevice {
+    public SmartLight(String name) {
+        super(name);
+        this.status = "off";
+    }
+
+    @Override
+    public void changeState(String newState) {
+        if (!isActive) {
+            System.out.println("Device is disconnected");
+            return;
+        }
+        if (newState.equalsIgnoreCase("on") || newState.equalsIgnoreCase("off")) {
+            this.status = newState.toLowerCase();
+            notifyObservers("Light is " + status);
+        }
+    }
 }
