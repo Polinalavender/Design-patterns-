4de4b73b740a67fb203c4e774cebb3a8915503d4
liafernando <91317153+liafernando@users.noreply.github.com>
2025-03-18 09:50:26 +0100

    Added the correct package

--- Smart-Home-App/src/main/java/device/SmartThermostat.java
+++ Smart-Home-App/src/main/java/device/SmartThermostat.java
@@ -1,4 +1,18 @@
-package device;
+package com.smarthome.model.device;
 
-public class SmartThermostat {
+public class SmartThermostat extends SmartDevice {
+    public SmartThermostat(String name) {
+        super(name);
+        this.status = "20°C";
+    }
+
+    @Override
+    public void changeState(String newState) {
+        if (!isActive) {
+            System.out.println("Device is disconnected");
+            return;
+        }
+        this.status = newState;
+        notifyObservers("Temperature set to " + status);
+    }
 }
