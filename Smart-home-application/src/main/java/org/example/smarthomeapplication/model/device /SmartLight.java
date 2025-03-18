c95c426bdc512a99a8098b82187036ca77ba6003
liafernando <91317153+liafernando@users.noreply.github.com>
2025-03-18 13:56:26 +0100

    devices updated in new project

---
+++ Smart-home-application/src/main/java/org/example/smarthomeapplication/model/device /SmartLight.java
@@ -0,0 +1,20 @@
+package com.smarthome.model.device;
+
+public class SmartLight extends SmartDevice {
+    public SmartLight(String name) {
+        super(name);
+        this.status = "off";
+    }
+
+    @Override
+    public void changeState(String newState) {
+        if (!isActive) {
+            System.out.println("Device is disconnected");
+            return;
+        }
+        if (newState.equalsIgnoreCase("on") || newState.equalsIgnoreCase("off")) {
+            this.status = newState.toLowerCase();
+            notifyObservers("Light is " + status);
+        }
+    }
+}
