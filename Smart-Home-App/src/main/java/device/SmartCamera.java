b8b6aa512deee5f7ba74d98a75530a31669c194c
liafernando <91317153+liafernando@users.noreply.github.com>
2025-03-18 09:48:34 +0100

    smart camera feature

--- Smart-Home-App/src/main/java/device/SmartCamera.java
+++ Smart-Home-App/src/main/java/device/SmartCamera.java
@@ -1,4 +1,20 @@
-package device;
+package com.smarthome.model.device;
 
-public class SmartCamera {
+public class SmartCamera extends SmartDevice {
+    public SmartCamera(String name) {
+        super(name);
+        this.status = "off";
+    }
+
+    @Override
+    public void changeState(String newState) {
+        if (!isActive) {
+            System.out.println("Device is disconnected");
+            return;
+        }
+        if (newState.equalsIgnoreCase("on") || newState.equalsIgnoreCase("off")) {
+            this.status = newState.toLowerCase();
+            notifyObservers("Camera recording is " + status);
+        }
+    }
 }
